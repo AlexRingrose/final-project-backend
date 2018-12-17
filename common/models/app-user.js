82bf0a2bc64d96fc3dfb999441a198387f9be36c
AlexRingrose <43159722+AlexRingrose@users.noreply.github.com>
2018-12-17 00:39:37 +0000

    update

--- common/models/app-user.js
+++ common/models/app-user.js
@@ -7,4 +7,22 @@ module.exports = function(Appuser) {
             user.token = user.id;
         }
     })
+    
+    
+    Appuser.observe('after save', function(ctx, next) {
+      if (ctx.isNewInstance === true) {
+        var instance = ctx.instance;
+         instance.createAccessToken(1209600000, function(err, response){
+            if (err === null) {
+               ctx.instance['userId'] = response.userId
+               ctx.instance["token"] = response.id;
+             }
+              next();
+           });
+            }
+            else {
+                next();
+            }
+        });
 };
+
